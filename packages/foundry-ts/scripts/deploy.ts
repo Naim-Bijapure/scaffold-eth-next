ab5a720a62220e15703b14a627a2a1dda35d38ad
naimbijapure <naimbijapure7407@gmail.com>
2022-06-13 04:20:03 +0000

    eslint husky config update

--- packages/foundry-ts/scripts/deploy.ts
+++ packages/foundry-ts/scripts/deploy.ts
@@ -1,118 +1,124 @@
-import { spawn } from "node:child_process";
-import { TNetworkNames } from "@scaffold-eth/common/src/models/TNetworkNames";
-import chalk from "chalk";
 import fs from "fs";
+import { spawn } from "node:child_process";
+import path from "node:path";
 
 import { NETWORKS } from "@scaffold-eth/common/src/constants/networks";
+import { TNetworkNames } from "@scaffold-eth/common/src/models/TNetworkNames";
+import chalk from "chalk";
 import dotenv from "dotenv";
-
-import account from "../generated/account.json";
-import { DEPLOY_CONTRACTS } from "../configs";
 import { ethers } from "ethers";
-import path from "node:path";
+
+import { DEPLOY_CONTRACTS } from "../configs";
+import account from "../generated/account.json";
 import sendBalanceToLocalAddress from "../helpers/sendBalanceToLocalAddress";
 
-//load root .env file
+// load root .env file
 dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 const network = args.length === 0 ? ("localhost" as TNetworkNames) : (args[1] as TNetworkNames);
 console.log(chalk.blueBright(`deploying to network => ${network}`));
-let address = account.address;
-let privateKey = account.privateKey;
+const address = account.address;
+const privateKey = account.privateKey;
 // const CONTRACTS_FILE = "./generated/foundry_contracts.json";
 const CONTRACTS_FILE = "../next-ts/contracts/foundry_contracts.json";
 
-const deploy = async (contractName: string, { args }: { args: string[] }) => {
-    let currentNetwork = NETWORKS[network];
-    let networkUrl = currentNetwork.url;
+const deploy = async (contractName: string, { args }: { args: string[] }): Promise<any> => {
+  const currentNetwork = NETWORKS[network];
+  const networkUrl = currentNetwork.url;
 
-    return new Promise((resolve, reject) => {
-        const deploy = spawn("forge", [
-            "create",
-            "--rpc-url",
-            networkUrl,
-            "--private-key",
-            privateKey,
-            `${contractName}`,
-            "--constructor-args",
-            ...args,
-            network !== "localhost" ? "--verify" : "",
-        ]);
-        deploy.stdout.on("data", (data) => {
-            if (data.toString().includes("Deployed to:")) {
-                let output = data.toString();
+  return new Promise((resolve, reject) => {
+    const deploy = spawn("forge", [
+      "create",
+      "--rpc-url",
+      networkUrl,
+      "--private-key",
+      privateKey,
+      `${contractName}`,
+      "--constructor-args",
+      ...args,
+      network !== "localhost" ? "--verify" : "",
+    ]);
+    deploy.stdout.on("data", (data) => {
+      if (data.toString().includes("Deployed to:")) {
+        const output = data.toString();
 
-                let deployedCotractAddress = output.split(":")[2];
-                deployedCotractAddress = String(deployedCotractAddress).split("\n")[0].trim();
-                console.log(`Contract => ${contractName} is Deployed at => ${deployedCotractAddress}`);
-                let contractData = {
-                    [currentNetwork.chainId]: { contracts: { [contractName]: { address: deployedCotractAddress } } },
-                };
+        let deployedCotractAddress = output.split(":")[2];
+        deployedCotractAddress = String(deployedCotractAddress).split("\n")[0].trim();
+        console.log(`Contract => ${contractName} is Deployed at => ${deployedCotractAddress}`);
+        const contractData = {
+          [currentNetwork.chainId]: {
+            contracts: { [contractName]: { address: deployedCotractAddress } },
+          },
+        };
 
-                const isExists = fs.existsSync(CONTRACTS_FILE);
-                if (isExists == false) {
-                    fs.writeFileSync(CONTRACTS_FILE, JSON.stringify(contractData, null, 4), "utf8");
-                }
-
-                if (isExists) {
-                    let oldFileData = fs.readFileSync(CONTRACTS_FILE);
-                    let contractsData = JSON.parse(oldFileData.toString());
-                    //     console.log("contractsData: ", JSON.stringify(contractsData));
-                    let currentContractData = { [contractName]: { address: deployedCotractAddress } };
-                    let isChainIdExist = contractsData[currentNetwork.chainId] ? true : false;
-                    if (isChainIdExist) {
-                        contractsData[currentNetwork.chainId]["contracts"] = {
-                            ...contractsData[currentNetwork.chainId]["contracts"],
-                            ...currentContractData,
-                        };
-                    } else {
-                        contractsData[currentNetwork.chainId] = {};
-                        contractsData[currentNetwork.chainId]["contracts"] = {
-                            ...currentContractData,
-                        };
-                    }
-
-                    fs.writeFileSync(CONTRACTS_FILE, JSON.stringify(contractsData, null, 4), "utf8");
-                }
-            }
-        });
-
-        deploy.stderr.on("data", (data) => {
-            console.log("data: ", data.toString());
-        });
-        deploy.on("exit", (code) => {
-            if (code === 0) {
-                resolve(true);
-            }
-            if (code !== 0) {
-                console.log(chalk.yellowBright("something went wrong"));
-                console.warn(chalk.redBright(`Try providing funds to generated account address => ${address}`));
-                console.log(chalk.blue.bgWhiteBright("goto https://punkwallet.io/"));
-            }
-        });
-    });
-};
-
-async function run() {
-    console.log(chalk.blueBright(`deploying...`));
-
-    if (network === "localhost") {
-        let localProvider = new ethers.providers.StaticJsonRpcProvider(process.env.STATIC_LOCAL_RPC);
-        let balanceBigNumber = await localProvider.getBalance(address);
-        let balance = ethers.utils.formatEther(balanceBigNumber).toString();
-        // if initial balance is zero at start
-        if (Number(balance) === 0) {
-            await sendBalanceToLocalAddress(address);
+        const isExists = fs.existsSync(CONTRACTS_FILE);
+        if (isExists === false) {
+          fs.writeFileSync(CONTRACTS_FILE, JSON.stringify(contractData, null, 4), "utf8");
         }
 
-        let balanceBigNumber1 = await localProvider.getBalance(address);
-        let balance1 = ethers.utils.formatEther(balanceBigNumber1).toString();
+        if (isExists) {
+          const oldFileData = fs.readFileSync(CONTRACTS_FILE);
+          const contractsData = JSON.parse(oldFileData.toString());
+          //     console.log("contractsData: ", JSON.stringify(contractsData));
+          const currentContractData = {
+            [contractName]: { address: deployedCotractAddress },
+          };
+          const isChainIdExist = contractsData[currentNetwork.chainId] ? true : false;
+          if (isChainIdExist) {
+            contractsData[currentNetwork.chainId]["contracts"] = {
+              ...contractsData[currentNetwork.chainId]["contracts"],
+              ...currentContractData,
+            };
+          } else {
+            contractsData[currentNetwork.chainId] = {};
+            contractsData[currentNetwork.chainId]["contracts"] = {
+              ...currentContractData,
+            };
+          }
+
+          fs.writeFileSync(CONTRACTS_FILE, JSON.stringify(contractsData, null, 4), "utf8");
+        }
+      }
+    });
+
+    deploy.stderr.on("data", (data) => {
+      console.log("data: ", data.toString());
+    });
+    deploy.on("exit", (code) => {
+      if (code === 0) {
+        resolve(true);
+      }
+      if (code !== 0) {
+        console.log(chalk.yellowBright("something went wrong"));
+        console.warn(chalk.redBright(`Try providing funds to generated account address => ${address}`));
+        console.log(chalk.blue.bgWhiteBright("goto https://punkwallet.io/"));
+      }
+    });
+  });
+};
+
+async function run(): Promise<any> {
+  console.log(chalk.blueBright(`deploying...`));
+
+  if (network === "localhost") {
+    const localProvider = new ethers.providers.StaticJsonRpcProvider(process.env.STATIC_LOCAL_RPC);
+    const balanceBigNumber = await localProvider.getBalance(address);
+    const balance = ethers.utils.formatEther(balanceBigNumber).toString();
+    // if initial balance is zero at start
+    if (Number(balance) === 0) {
+      await sendBalanceToLocalAddress(address);
     }
 
-    for (const contract of DEPLOY_CONTRACTS) {
-        await deploy(contract.contractName, { args: [...contract.args] });
-    }
+    const balanceBigNumber1 = await localProvider.getBalance(address);
+    const balance1 = ethers.utils.formatEther(balanceBigNumber1).toString();
+  }
+
+  for (const contract of DEPLOY_CONTRACTS) {
+    await deploy(contract.contractName, { args: [...contract.args] });
+  }
 }
-run();
+run().catch((error) => {
+  console.log("error: ", error);
+});
--- packages/next-ts/pages/index.tsx
+++ packages/next-ts/pages/index.tsx
@@ -7,79 +7,78 @@ import { useAccount, useBalance } from "wagmi";
 
 import useAppLoadContract from "../hooks/useAppLoadContract";
 const Home: NextPage = () => {
-    const [contractPurpose, setContractPurpose] = useState<string>("");
+  const [contractPurpose, setContractPurpose] = useState<string>("");
 
-    const { data: accountData, isLoading } = useAccount();
-    const { data } = useBalance({ addressOrName: accountData?.address });
+  const { data: accountData, isLoading } = useAccount();
+  const { data } = useBalance({ addressOrName: accountData?.address });
 
-    const YourContract = useAppLoadContract({
-        contractName: "YourContract",
-    });
+  const YourContract = useAppLoadContract({
+    contractName: "YourContract",
+  });
 
-    const getPurpose = useCallback(async () => {
-        let purpose = await YourContract?.purpose();
-        setContractPurpose(purpose as string);
-    }, [YourContract]);
+  const getPurpose = useCallback(async () => {
+    const purpose = await YourContract?.purpose();
+    setContractPurpose(purpose as string);
+  }, [YourContract]);
 
-    useEffect(() => {
-        void getPurpose();
-    }, [YourContract, getPurpose]);
+  useEffect(() => {
+    void getPurpose();
+  }, [YourContract, getPurpose]);
 
-    return (
-        <>
-            <main className="m-2 lg:mx-4 flex flex-col items-start justify-center ">
-                <div className="m-2">
-                    <span className="mx-2">📝</span>
-                    This Is Your App Home. You can start editing it in
-                    <span className="highight">/pages </span> folder
-                </div>
+  return (
+    <>
+      <main className="flex flex-col items-start justify-center m-2 lg:mx-4">
+        <div className="m-2">
+          <span className="mx-2">📝</span>
+          This Is Your App Home. You can start editing it in
+          <span className="highight">/pages </span> folder
+        </div>
 
-                <div className="m-2">
-                    <span className="mx-2">✏️</span>
-                    Edit your smart contract <span className="bg-base-200 mx-2">YourContract.sol</span> in{" "}
-                    <span className="highight">packages/foundry-hardat-ts/src</span>
-                </div>
-                {!contractPurpose ? (
-                    <div className="m-2">
-                        <span className="mx-2">👷‍♀️</span>
-                        You haven&apos;t deployed your contract yet, run
-                        <span className="bg-base-200 mx-2">yarn chain</span> and{" "}
-                        <span className="bg-base-200 mx-2">yarn deploy</span> to deploy your first contract!
-                    </div>
-                ) : (
-                    <div className="m-2">
-                        <span className="mx-2">🤓</span>
-                        The &quot;purpose&quot; variable from your contract is{" "}
-                        <span className="bg-base-200 mx-2">{contractPurpose}</span>
-                    </div>
-                )}
+        <div className="m-2">
+          <span className="mx-2">✏️</span>
+          Edit your smart contract <span className="mx-2 bg-base-200">YourContract.sol</span> in{" "}
+          <span className="highight">packages/foundry-hardat-ts/src</span>
+        </div>
+        {!contractPurpose ? (
+          <div className="m-2">
+            <span className="mx-2">👷‍♀️</span>
+            You haven&apos;t deployed your contract yet, run
+            <span className="mx-2 bg-base-200">yarn chain</span> and{" "}
+            <span className="mx-2 bg-base-200">yarn deploy</span> to deploy your first contract!
+          </div>
+        ) : (
+          <div className="m-2">
+            <span className="mx-2">🤓</span>
+            The &quot;purpose&quot; variable from your contract is{" "}
+            <span className="mx-2 bg-base-200">{contractPurpose}</span>
+          </div>
+        )}
 
-                <div className="m-2">
-                    <span className="mx-2">🤖</span>
-                    An example to get your balance from wagmi hooks for your account address
-                    <span className="bg-base-200 mx-2">{accountData?.address}</span>
-                    balance :{" "}
-                    <span className="bg-base-200 mx-2">{data && formatEther(data?.value as BigNumberish)} eth</span>
-                </div>
-                <div className="m-2">
-                    <span className="mx-2">💭</span>
-                    Check out the{" "}
-                    <Link href={"/Hints"}>
-                        <span className="link link-primary">Hints</span>
-                    </Link>{" "}
-                    tab for more tips.
-                </div>
-                <div className="m-2">
-                    <span className="m-2">🛠</span>
-                    Tinker with your smart contract using the
-                    <Link href={"/Debug"}>
-                        <span className="link link-primary mx-2">Debug</span>
-                    </Link>{" "}
-                    tab.
-                </div>
-            </main>
-        </>
-    );
+        <div className="m-2">
+          <span className="mx-2">🤖</span>
+          An example to get your balance from wagmi hooks for your account
+          <span className="mx-2 bg-base-200">{accountData?.address}</span>
+          balance : <span className="mx-2 bg-base-200">{data && formatEther(data?.value as BigNumberish)} eth</span>
+        </div>
+        <div className="m-2">
+          <span className="mx-2">💭</span>
+          Check out the{" "}
+          <Link href={"/Hints"}>
+            <span className="link link-primary">Hints</span>
+          </Link>{" "}
+          tab for more tips.
+        </div>
+        <div className="m-2">
+          <span className="m-2">🛠</span>
+          Tinker with your smart contract using the
+          <Link href={"/Debug"}>
+            <span className="mx-2 link link-primary">Debug</span>
+          </Link>{" "}
+          tab.
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
