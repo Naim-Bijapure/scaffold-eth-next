230fc1d2d5b3fc540f105cef50dcfabd44a188b0
NaimBijapure <naim@vinnovatetechnologies.com>
2022-07-08 12:37:46 +0530

    Title: hotfix:macos foundry installation

--- packages/foundry-ts/foundry-init.mjs
+++ packages/foundry-ts/foundry-init.mjs
@@ -9,6 +9,12 @@ let isFoundryExist = fs.existsSync("/root/.foundry/bin/foundryup");
 // for linux and macos installation
 if (["linux", "darwin"].includes(platform)) {
   if (isFoundryExist === false) {
+    // for macos install libusb package
+    if (platform === "darwin") {
+      console.log(chalk.blue("you are on mac installing libusb package"));
+      await $`brew install libusb`;
+    }
+
     await spinner("wait downloading foundry", () => $`curl -L https://foundry.paradigm.xyz | bash`);
     console.log(chalk.blue("installing foundry"));
 
@@ -26,10 +32,12 @@ if (["linux", "darwin"].includes(platform)) {
     console.log(
       chalk.bgYellowBright(
         chalk.black(
-          `if you are not on bash shell add  this path line => {export PATH="$PATH:/root/.foundry/bin"} and source it \n`
+          `if you are not on bash shell add  this path line => {export PATH="$PATH:/user_path/.foundry/bin"} and source it \n`
         )
       )
     );
+
+    console.log(chalk.bgYellowBright(chalk.black(`for zsh eg: source ~/.zshrc \n`)));
 
     console.log(chalk.bgGreen(chalk.white(`run => yarn scafold:help to get basic commands \n`)));
   }
