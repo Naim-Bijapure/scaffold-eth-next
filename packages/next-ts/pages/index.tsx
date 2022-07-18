5b22291e0013dc4decfe42af414526558458dc5e
ldsanchez <lsanchez.ve@gmail.com>
2022-07-18 00:48:01 -0500

    add consistent style

--- packages/next-ts/pages/index.tsx
+++ packages/next-ts/pages/index.tsx
@@ -27,54 +27,57 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <main className="flex flex-col items-start justify-center m-2 lg:mx-4">
-        <div className="m-2">
-          <span className="mx-2">📝</span>
-          This Is Your App Home. You can start editing it in
-          <span className="highight">/pages </span> folder
+      <main>
+        <div className="flex flex-col flex-no-wrap items-center content-center justify-center">
+        <div className="m-8">
+          <span className="mr-2">📝</span>
+          This Is Your App Home. You can start editing it in{" "}
+          <span className="font-bold bg-primary-content">packages/next-ts/pages/index.tsx</span> folder
         </div>
 
-        <div className="m-2">
-          <span className="mx-2">✏️</span>
-          Edit your smart contract <span className="mx-2 bg-base-200">YourContract.sol</span> in{" "}
-          <span className="highight">packages/foundry-hardat-ts/src</span>
+        <div className="m-8">
+          <span className="mr-2">✏️</span>
+          Edit your smart contract <span className="font-bold bg-primary-content">YourContract.sol</span> in{" "}
+          <span className="font-bold bg-primary-content">packages/foundry-ts/src</span>
         </div>
         {!contractPurpose ? (
-          <div className="m-2">
-            <span className="mx-2">👷‍♀️</span>
-            You haven&apos;t deployed your contract yet, run
-            <span className="mx-2 bg-base-200">yarn chain</span> and{" "}
-            <span className="mx-2 bg-base-200">yarn deploy</span> to deploy your first contract!
+          <div className="m-8">
+            <span className="mr-2">👷‍♀️</span>
+            You haven&apos;t deployed your contract yet, run{" "}
+            <span className="font-bold bg-primary-content">yarn chain</span> and{" "}
+            <span className="font-bold bg-primary-content">yarn deploy</span> to deploy your first contract!
           </div>
         ) : (
-          <div className="m-2">
-            <span className="mx-2">🤓</span>
+          <div className="m-8">
+            <span className="mr-2">🤓</span>
             The &quot;purpose&quot; variable from your contract is{" "}
-            <span className="mx-2 bg-base-200">{contractPurpose}</span>
+            <span className="font-bold bg-primary-content">{contractPurpose}</span>
           </div>
         )}
 
-        <div className="m-2">
-          <span className="mx-2">🤖</span>
-          An example to get your balance from wagmi hooks for your account
-          <span className="mx-2 bg-base-200">{accountData?.address}</span>
-          balance : <span className="mx-2 bg-base-200">{data && formatEther(data?.value as BigNumberish)} eth</span>
+        <div className="m-8">
+          <span className="mr-2">🤖</span>
+          An example to get your balance:{" "}
+          <span className="font-bold text-green-900">{data && formatEther(data?.value as BigNumberish)}</span>{" "}for address{" "}
+          <span className="font-bold bg-primary-content">{accountData?.address}</span>
+          {" "}from wagmi hooks!
         </div>
-        <div className="m-2">
-          <span className="mx-2">💭</span>
+        <div className="m-8">
+          <span className="mr-2">💭</span>
           Check out the{" "}
           <Link href={"/Hints"}>
             <span className="link link-primary">Hints</span>
           </Link>{" "}
           tab for more tips.
         </div>
-        <div className="m-2">
-          <span className="m-2">🛠</span>
+        <div className="m-8">
+          <span className="mr-2">🛠</span>
           Tinker with your smart contract using the
           <Link href={"/Debug"}>
             <span className="mx-2 link link-primary">Debug</span>
           </Link>{" "}
           tab.
+        </div>
         </div>
       </main>
     </>
